8d591087e12b132931c39f206c01c2aa8addb508
Andriana Brockman <andriana.brockman@gmail.com>
2020-07-21 01:09:27 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,7 +3,7 @@ def position_taken?(board, index)
     if board[index] == " " || "" || nil
       return false 
       
-    elsif board[index] == "X" || "O"
+    elsif 
        return true
     
     end   
